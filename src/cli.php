67959f9bbb6fb377395c483096391a4b52ce1c22
Vladimir Loskutov <vladimirloskutov@icloud.com>
2019-10-08 16:40:00 +0500

    add phpcs recommendations (comments)

--- src/cli.php
+++ src/cli.php
@@ -1,10 +1,17 @@
 <?php
 
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+
 namespace BrainGames\Cli;
 
 use function \cli\line;
 use function \cli\prompt;
 
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
 function run()
 {
     line('Welcome to the Brain Game!');
