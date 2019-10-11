672aa07c7ae1fea62b58a59e336ea0cc9822fa14
Vladimir Loskutov <vladimirloskutov@icloud.com>
2019-10-11 22:32:29 +0500

    update src/even.php

--- src/even.php
+++ src/even.php
@@ -19,5 +19,26 @@ function run()
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 
+    $stepNumber = 1;
 
+    while ($stepNumber < 4) {
+        $value = rand(1, 99);
+        line('Question: %s', $value);
+        $answer = prompt('Your answer');
+
+        $correctAnswer = ($value % 2 === 0 ? 'yes' : 'no');
+
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            break;
+        }
+        $stepNumber++;
+
+        if ($stepNumber === 4) {
+            line("Congratulations, %s!", $name);
+        }
+    }
 }
