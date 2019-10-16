cfae9aa41ab17a8c24c200cff8e19771105890b0
Vladimir Loskutov <vladimirloskutov@icloud.com>
2019-10-16 22:40:25 +0500

    fix logic of even game

--- src/engine.php
+++ src/engine.php
@@ -9,12 +9,34 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
+CONST COUNT_ROUNDS = 3;
+
 /**
  * Актуализировать комментарий перед окончанием проекта
  */
-function run()
+function run($description, $gameData)
 {
     line('Welcome to the Brain Game!');
+    line($description);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    for ($roundNumber = 1; $roundNumber <= COUNT_ROUNDS; $roundNumber++) {
+        $value = $gameData();
+        line('Question: %s', $value[0]);
+        $answer = prompt('Your answer');
+
+        $correctAnswer = $value[1];
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            break;
+        }
+    }
+
+    if ($roundNumber === 4) {
+        line("Congratulations, %s!", $name);
+    }
 }
--- src/even.php
+++ src/even.php
@@ -6,39 +6,22 @@
 
 namespace BrainGames\Even;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Engine\run as runGame;
+
+const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 /**
  * Актуализировать комментарий перед окончанием проекта
  */
 function run()
 {
-    line('Welcome to the Brain Game!');
-    line('Answer "yes" if the number is even, otherwise answer "no".');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    $getGameDataFunction = function()
+    {
+        $result[] = rand(1, 100);
+        $result[] = ($result[0] % 2 === 0 ? 'yes' : 'no');
 
-    $stepNumber = 1;
+        return $result;
+    };
 
-    while ($stepNumber < 4) {
-        $value = rand(1, 99);
-        line('Question: %s', $value);
-        $answer = prompt('Your answer');
-
-        $correctAnswer = ($value % 2 === 0 ? 'yes' : 'no');
-
-        if ($answer === $correctAnswer) {
-            line('Correct!');
-        } else {
-            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
-            line("Let's try again, %s!", $name);
-            break;
-        }
-        $stepNumber++;
-
-        if ($stepNumber === 4) {
-            line("Congratulations, %s!", $name);
-        }
-    }
+    runGame(DESCRIPTION, $getGameDataFunction);
 }
