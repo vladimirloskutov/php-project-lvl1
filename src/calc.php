35801633ab05338f1d8779a89e309322d3e8ff5b
Vladimir Loskutov <vladimirloskutov@icloud.com>
2019-10-17 22:27:10 +0500

    add calc.php

---
+++ src/calc.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+
+namespace BrainGames\Calc;
+
+use function BrainGames\Engine\run as runGame;
+
+const DESCRIPTION = 'What is the result of the expression?';
+
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+function run()
+{
+    $getGameDataFunction = function()
+    {
+        $numbers[] = rand(1, 100);
+        $numbers[] = rand(1, 100);
+        $operators = ['+', '-', '*'];
+        $operation = array_rand($operators, 1);
+
+        if ($operators[$operation] === '+') {
+            $result[] = "{$numbers[0]} {$operators[$operation]} {$numbers[1]}";
+            $result[] = $numbers[0] + $numbers[1];
+        } elseif ($operators[$operation] === '-') {
+            $result[] = "{$numbers[0]} {$operators[$operation]} {$numbers[1]}";
+            $result[] = $numbers[0] - $numbers[1];
+        } else {
+            $result[] = "{$numbers[0]} {$operators[$operation]} {$numbers[1]}";
+            $result[] = $numbers[0] * $numbers[1];
+        }
+
+        return $result;
+    };
+
+    runGame(DESCRIPTION, $getGameDataFunction);
+}
