a9a700747b6af942b71400f4be12c69055742d14
Vladimir Loskutov <vladimirloskutov@icloud.com>
2019-10-20 22:29:59 +0500

    add progression game

---
+++ src/progression.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+
+namespace BrainGames\Progression;
+
+use function BrainGames\Engine\run as runGame;
+
+const DESCRIPTION = 'What number is missing in the progression?';
+
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+function run()
+{
+    $getGameDataFunction = function()
+    {
+        $firstMember = rand(1, 100);
+        $difference = rand(1, 10);
+        $progression[] = $firstMember;
+
+        $lengthOfProgression = 10;
+
+        for ($i = 1; $i < $lengthOfProgression; $i++) {
+
+            $progression[] = $progression[$i - 1] + $difference;
+        }
+
+        $randomKey = array_rand($progression);
+        $result[1] = $progression[$randomKey];
+        $progression[$randomKey] = '..';
+        $result[0] = implode(' ', $progression);
+
+        return $result;
+    };
+
+    runGame(DESCRIPTION, $getGameDataFunction);
+}
