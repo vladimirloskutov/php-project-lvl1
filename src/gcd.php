c926d56dbed8532f666a44f565ff294aa0ae6d9d
Vladimir Loskutov <vladimirloskutov@icloud.com>
2019-10-20 13:48:55 +0500

    add gcd game

---
+++ src/gcd.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+
+namespace BrainGames\Gcd;
+
+use function BrainGames\Engine\run as runGame;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+/**
+ * Актуализировать комментарий перед окончанием проекта
+ */
+function run()
+{
+    $getGameDataFunction = function()
+    {
+        $numbers[] = rand(1, 100);
+        $numbers[] = rand(1, 100);
+
+        $result[] = "{$numbers[0]} {$numbers[1]}";
+
+        if ($numbers[0] > $numbers[1]) {
+            $maxNumber = $numbers[0];
+            $minNumber = $numbers[1];
+        } else {
+            $maxNumber = $numbers[1];
+            $minNumber = $numbers[0];
+        }
+
+        for ($i = $minNumber; $i > 0; $i--) {
+
+            if ($maxNumber % $i === 0 && $minNumber % $i === 0) {
+                $result[] = $i;
+                break;
+            }
+        }
+
+        return $result;
+    };
+
+    runGame(DESCRIPTION, $getGameDataFunction);
+}
